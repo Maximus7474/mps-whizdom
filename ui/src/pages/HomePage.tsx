ace12247f131acd3b33bf443d1fea40b2b96d940
Maximus7474 <the.maximus.7474@gmail.com>
2025-11-20 16:51:02 +0100

    chore(ui/HomePage): updated homepage layout

--- ui/src/pages/HomePage.tsx
+++ ui/src/pages/HomePage.tsx
@@ -1,9 +1,14 @@
 import { IPageProps } from "@/types";
 
+import './HomePage.css';
+
 const HomePage = ({ device }: IPageProps) => {
-    return <>
-        <h1>Welcome to <span style={{ fontStyle: 'italic'}}>WhizDom</span> !</h1>
-    </>
+    return <div className="homepage">
+        <div className="home-header">
+            <h1 className={device}>Welcome to</h1>
+            <img src='/TextBanner.png' className={`text-banner ${device}`} />
+        </div>
+    </div>
 }
 
 export default HomePage;
