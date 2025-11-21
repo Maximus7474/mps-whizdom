424cfb5538eb45fa4b84e70e20638df57b8f2b67
Maximus7474 <the.maximus.7474@gmail.com>
2025-11-22 00:08:22 +0100

    feat(config): added fields regarding word selection

--- config.lua
+++ config.lua
@@ -10,6 +10,19 @@ Config.Name = "WhizDom"
 Config.Description = "Show your wisdom by guessing words."
 Config.Developer = "Whizz" -- GTA V lore company
 
+-- Locale used for the word generation
+Config.Locale = {
+    Code = 'en',
+    -- If a user is using another locale, an adequate word will show.
+    -- So if the Code is set to 'en' but the user has german on his phone, he'll get a german word.
+    AllowVariousLocales = false,
+}
+
+Config.WordSelection = {
+    Length = 5,
+    Category = nil, -- 'wordle' | 'brainrot' | 'countries' (for more refer to https://random-words-api.kushcreates.com)
+}
+
 Config.Screenshots = {
     Phone = {
         "screenshots/phone/screenshot-dark.webp",
