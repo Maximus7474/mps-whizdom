7fde0ea45cb8c3a0b5c6310b38f859c5638f97d6
Maximus7474 <the.maximus.7474@gmail.com>
2025-11-21 11:24:33 +0100

    refactor(shared/utils): added cache to IsResourceAvailable

--- shared/utils.lua
+++ shared/utils.lua
@@ -1,8 +1,21 @@
+local resourceCache = {}
+
 ---Check if a resource is started and present
 ---@param resource string
 ---@return boolean
 function IsResourceAvailable(resource)
-    local state = GetResourceState(resource)
+    local state = resourceCache[resource]
+
+    if not state then
+        state = GetResourceState(resource)
+        resourceCache[resource] = state
+    end
 
     return state == 'started' or state == 'starting'
 end
+
+AddEventHandler('onResourceStop', function (resource)
+    if resourceCache[resource] then
+        resourceCache[resource] = nil
+    end
+end)
