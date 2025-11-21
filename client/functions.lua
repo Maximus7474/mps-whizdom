2c37502c4d91fccdd889832be6c3c655556ca999
Maximus7474 <the.maximus.7474@gmail.com>
2025-11-21 11:28:16 +0100

    refactor(client/functions): trigger callback depending on current device

--- client/functions.lua
+++ client/functions.lua
@@ -3,9 +3,9 @@
 ---@param ... unknown
 ---@return unknown
 function AwaitCallback(event, ...)
-    if IsResourceAvailable('lb-phone') then
+    if CURRENT_DEVICE == "phone" and IsResourceAvailable('lb-phone') then
         return exports["lb-phone"]:AwaitCallback(event, ...)
-    elseif IsResourceAvailable('lb-tablet') then
+    elseif CURRENT_DEVICE == "tablet" and IsResourceAvailable('lb-tablet') then
         return exports["lb-tablet"]:AwaitCallback(event, ...)
     else
         error('Nor lb-tablet or lb-phone is present, this resource will not work', 0)
