b35e8c7f01044f0e97fc171bb223b5e90258427d
Maximus7474 <the.maximus.7474@gmail.com>
2025-11-22 12:40:50 +0100

    fix(server/functions): broken RegisterCallback Wrapper

--- server/functions.lua
+++ server/functions.lua
@@ -5,14 +5,16 @@
 ---@param antiSpam? boolean
 function RegisterCallback(event, handler, defaultReturn, antiSpam)
     if IsResourceAvailable('lb-phone') then
-        exports["lb-phone"]:RegisterCallback(event, function (source, phoneNumber, ...)
-            handler(source, "phone", phoneNumber, ...)
+        exports["lb-phone"]:RegisterCallback(event, function (source, ...)
+            local phoneNumber = exports["lb-phone"]:GetEquippedPhoneNumber(source)
+            return handler(source, "phone", phoneNumber, ...)
         end, defaultReturn, antiSpam)
     end
 
     if IsResourceAvailable('lb-tablet') then
-        exports["lb-tablet"]:RegisterCallback(event, function (source, tabletId, ...)
-            handler(source, "tablet", tabletId, ...)
+        exports["lb-tablet"]:RegisterCallback(event, function (source, ...)
+            local tabletId = exports["lb-tablet"]:GetEquippedTablet(source)
+            return handler(source, "tablet", tabletId, ...)
         end, defaultReturn, antiSpam)
     end
 end
